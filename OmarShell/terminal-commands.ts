5a3d27f11de4e9d5ca83921f69da237faf345868
salem5 <salem5@outlook.com>
2014-12-29 17:07:47 +0100

    Added WCF Service, added marco command to test it. Minor changes.

--- OmarShell/terminal-commands.ts
+++ OmarShell/terminal-commands.ts
@@ -1,11 +1,8 @@
 ﻿var myCommands;
 
 var commandExecution = function (command, term) {
-    var splittedCommand = command.split(" ");
-
-    myCommands.callCommand(term, splittedCommand);
-    //term.echo('you type command "' + command + '"');
-    //term.resize();
+    //var splittedCommand = command.split(" ");
+    myCommands.callCommand(term, command);
     window.scrollTo(0, term.height());
 }
 
@@ -24,15 +21,17 @@ HHHHH. .HHHHH        //     ______    \\\n\
 \n\
 Welcome to Omar Shell, I am Omar Ajerray and this is my little hackjob of an attempt to put out something unique as a website. The backend is still missing, and there are only a few supported commands, but feel free to play around.\n\
 Find more about me online on my LinkedIn profile <http://uk.linkedin.com/in/omarajerray>\n\
-This Terminal uses <http://terminal.jcubic.pl> And everything else I coded myself with a healty mix of JQuery, TypeScript and CSS.\n";
+This terminal uses <http://terminal.jcubic.pl> and everything else I coded myself with a healty mix of JQuery, TypeScript and CSS.\n\
+The jquery definition for typescript comes from <http://definitelytyped.org>\n\
+One important thing, the terminal needs the focus to work. If the cursor blinks, everything is alright. If not, click on this text to pass focus to the terminal.";
 }
 
 class AvailableCommand {
-    command: String;
-    action: (argument: String[], terminal: any) => boolean;
+    commandName: String;
+    action: (fullcommand: String, terminal: any) => boolean;
     
-    constructor(argCommand: String, argAction: (argument: String[], terminal: any) => boolean) {
-        this.command = argCommand;
+    constructor(argCommandName: String, argAction: (fullCommand: String, terminal: any) => boolean) {
+        this.commandName = argCommandName;
         this.action = argAction;
     }
 }
@@ -44,12 +43,12 @@ class CommandCollection {
         this.commands.push(commandToAdd);
     }
 
-    callCommand(terminal: any, calledCommand: String[]): boolean {
+    callCommand(terminal: any, calledCommand: String): boolean {
         var result = false;
+        var command = calledCommand.split(" ", 2)[0];
 
         this.commands.forEach(function (vc) {
-            if (vc.command == calledCommand[0]) {
-                calledCommand.shift();
+            if (vc.commandName == command) {
                 vc.action(calledCommand, terminal);
                 result = true;
             }
@@ -66,23 +65,73 @@ class CommandCollection {
 window.onload = () => {
     myCommands = new CommandCollection();
     //about command for intro.
-    myCommands.addCommand(new AvailableCommand("about", function (argument,terminal) : boolean {
+    myCommands.addCommand(new AvailableCommand("about", function (command,terminal) : boolean {
         terminal.greetings();
         return true;
     }));
     //help command which shows available commands.
-    myCommands.addCommand(new AvailableCommand("help", function (argument, terminal): boolean {
+    myCommands.addCommand(new AvailableCommand("help", function (command, terminal): boolean {
         terminal.echo(
             "help - shows this help screen.\n" +
             "about - shows the intro information.\n" +
             "clear - clears the terminal of its history\n" +
-            "echo - (experimental) returns the arguments to the terminal."
+            "echo - (experimental) returns the arguments to the terminal." +
+            "marco - (experimental) testing WCF Service."
             );
         return true;
     }));
     //help command which shows available commands.
-    myCommands.addCommand(new AvailableCommand("echo", function (argument, terminal): boolean {
-        terminal.echo(argument);
+    myCommands.addCommand(new AvailableCommand("echo", function (command, terminal): boolean {
+        terminal.echo(command);
+        return true;
+    }));
+
+    //Testing WCF Service.
+    myCommands.addCommand(new AvailableCommand("marco", function (calledCommand, terminal): boolean {        
+        $.ajax({
+            type: "GET",
+            url: "Service1.svc/MarcoPolo",
+            data: { "command": calledCommand},
+            //data: JSON.stringify({ "command": calledCommand }),
+            dataType: "json",
+            //processData: false
+
+           // complete: marcoComplete,
+            //success: marcoSuccess
+
+            // Note: I am unsure how the signature looks for either done nor fail.
+        }).done(function (msg,a2,a3) {
+                terminal.echo(msg);     
+            terminal.echo(JSON.parse(a2));
+                //TODO: Add logging for other messages.
+          
+        }).fail(function (a1,a2,a3) { 
+            terminal.echo(a1);
+            terminal.echo(a2);
+            terminal.echo(a3);
+            });
         return true;
     }));
 };
+
+//var marcoSuccess = function (data: any, textStatus: String, jqXHR: JQueryXHR) {
+
+//    alert("successfunction");
+//    alert(data);
+//    alert(jqXHR);
+//    alert(textStatus);
+//}
+
+//var marcoComplete = function (jqXHR: JQueryXHR, textStatus: String) {
+
+//    alert("completefunction");
+//   alert(jqXHR);
+//   alert(textStatus);
+//}
+
+//var marcoError = function (jqXHR: JQueryXHR, textStatus: String, errorThrown: String) {
+
+//    alert(jqXHR);
+//    alert(textStatus);
+//    alert(errorThrown);
+//}
